23b7d60d0b2a5aa3634fa08642ffee8762822318
francescotrudu <francescotrudu84@gmail.com>
2022-11-25 16:06:51 +0100

    add document.write

--- script2.js
+++ script2.js
@@ -3,7 +3,9 @@
 const number = parseInt(prompt("Inserisci un numero"));
 
 if (number % 2 == 0) {
-    console.log(number)
+    console.log(number);
+    document.write(number);
 } else {
-    console.log(number + 1)
+    console.log(number + 1);
+    document.write(number + 1);
 }
