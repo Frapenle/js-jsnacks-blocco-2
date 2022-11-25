478915e18f15fd324c61e3fa3190b140cce3ef9f
francescotrudu <francescotrudu84@gmail.com>
2022-11-25 18:31:18 +0100

    Add comments

--- script3.js
+++ script3.js
@@ -4,15 +4,22 @@ const newArray = [3, 5, 64, -7, 4, 7, 88, -9, 0, 94, 33, 3, 4];
 
 let somma = 0;
 let arrayNum = 10;
+// assegnare all'array il primo valore del suo "index"
 let maxNum = newArray[0];
-console.log(maxNum);
+// console.log(maxNum);
 
 for (let i = 0; i < arrayNum; i++) {
+    // somma dei primi `arrayNum` numeri dell'array
     somma += parseInt(newArray[i]);
+
+    // trovare il valore piu grande tra i primi `arrayNum` dell array
+    // se `i` valore nell array è maggiore di `maxNum`
     if (newArray[i] > maxNum) {
+        // maxNum prende quel valore
         maxNum = newArray[i];
     }
 }
+
 // print somma
 console.log(somma);
 document.write("Somma " + somma);
